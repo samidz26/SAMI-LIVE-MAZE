af298b62b8a9b25a4d9368c928b61f601710b338
samidz26 <samizaouiadz@gmail.com>
2026-09-06 19:23:17 +0100

    Update gameManager.js

--- game/gameManager.js
+++ game/gameManager.js
@@ -2071,7 +2071,7 @@ function createGameManager({ io, settings }) {
             !cell.walls.bottom
         ) {
 
-            ny++;
+            ny--;
 
         } else if (
             command === "r" &&
@@ -2090,7 +2090,7 @@ function createGameManager({ io, settings }) {
              * سنصلحه لاحقًا بعد حل مشكلة المتاهة.
              */
 
-            nx++;
+            nx--;
 
         } else {
 
